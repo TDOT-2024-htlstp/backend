5e9ad2341332f957034321213824950df90298d5
Fabian <fabian.goetz06@gmail.com>
2024-11-06 10:26:39 +0100

    added Websocket exmaple with stompjs

---
+++ src/main/java/org/example/springbackend/domain/messages/HelloMessage.java
@@ -0,0 +1,16 @@
+package org.example.springbackend.domain.messages;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloMessage {
+
+    private String name;
+
+}
