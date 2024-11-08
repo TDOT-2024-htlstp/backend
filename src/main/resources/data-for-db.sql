0bdb78ad5bbaa59765eace6f0f4a7c12b736a0d0
Fabian <fabian.goetz06@gmail.com>
2024-11-08 11:20:45 +0100

    push

--- src/main/resources/data-for-db.sql
+++ src/main/resources/data-for-db.sql
@@ -1,40 +1,49 @@
 -- Categories
-INSERT INTO categories (id, name) VALUES (DEFAULT, 'Bonbons');
-INSERT INTO categories (id, name) VALUES (DEFAULT, 'Schokolade');
-INSERT INTO categories (id, name) VALUES (DEFAULT, 'Lutscher');
+-- INSERT INTO categories (id, name) VALUES (DEFAULT, 'Bonbons');
+-- INSERT INTO categories (id, name) VALUES (DEFAULT, 'Schokolade');
+-- INSERT INTO categories (id, name) VALUES (DEFAULT, 'Lutscher');
+
+    INSERT INTO categories (id, name) values (DEFAULT, 'Celebrations');
 
 -- Products
-INSERT INTO products (id, name, price, amount_left, image_path, category_id)
-VALUES (DEFAULT, 'Maoam Cola', 10, 20, 'api/images/maoam/cola.png', 1);
-INSERT INTO products (id, name, price, amount_left, image_path, category_id)
-VALUES (DEFAULT, 'Maoam Erdbeere', 10, 20, 'api/images/maoam/erdbeere.png', 1);
-INSERT INTO products (id, name, price, amount_left, image_path, category_id)
-VALUES (DEFAULT, 'Maoam Himbeere', 10, 20, 'api/images/maoam/himbeere.png', 1);
-INSERT INTO products (id, name, price, amount_left, image_path, category_id)
-VALUES (DEFAULT, 'Maoam Orange', 10, 20, 'api/images/maoam/orange.png', 1);
-INSERT INTO products (id, name, price, amount_left, image_path, category_id)
-VALUES (DEFAULT, 'Maoam Zitrone', 10, 20, 'api/images/maoam/zitrone.png', 1);
-INSERT INTO products (id, name, price, amount_left, image_path, category_id)
-VALUES (DEFAULT, 'Milka Mini', 10, 20, 'api/images/milka/minis.png', 2);
-INSERT INTO products (id, name, price, amount_left, image_path, category_id)
-VALUES (DEFAULT, 'Nimm 2 Lollipop', 30, 20, 'api/images/nimm2/lolli.png', 3);
+-- INSERT INTO products (id, name, price, amount_left, image_path, category_id)
+-- VALUES (DEFAULT, 'Maoam Cola', 10, 20, 'api/images/maoam/cola.png', 1);
+-- INSERT INTO products (id, name, price, amount_left, image_path, category_id)
+-- VALUES (DEFAULT, 'Maoam Erdbeere', 10, 20, 'api/images/maoam/erdbeere.png', 1);
+-- INSERT INTO products (id, name, price, amount_left, image_path, category_id)
+-- VALUES (DEFAULT, 'Maoam Himbeere', 10, 20, 'api/images/maoam/himbeere.png', 1);
+-- INSERT INTO products (id, name, price, amount_left, image_path, category_id)
+-- VALUES (DEFAULT, 'Maoam Orange', 10, 20, 'api/images/maoam/orange.png', 1);
+-- INSERT INTO products (id, name, price, amount_left, image_path, category_id)
+-- VALUES (DEFAULT, 'Maoam Zitrone', 10, 20, 'api/images/maoam/zitrone.png', 1);
+-- INSERT INTO products (id, name, price, amount_left, image_path, category_id)
+-- VALUES (DEFAULT, 'Milka Mini', 10, 20, 'api/images/milka/minis.png', 2);
+-- INSERT INTO products (id, name, price, amount_left, image_path, category_id)
+-- VALUES (DEFAULT, 'Nimm 2 Lollipop', 30, 20, 'api/images/nimm2/lolli.png', 3);
+
+    INSERT INTO products(id, name, price, amount_left, image_path, category_id)
+    VALUES (DEFAULT, 'Twix', 25, 145, 'api/images/celebrations/twix.png', 1),
+            (DEFAULT, 'Snickers', 25, 145, 'api/images/celebrations/snickers.png', 1),
+            (DEFAULT, 'Mars', 25, 145, 'api/images/celebrations/mars.png', 1),
+            (DEFAULT, 'Bounty', 25, 145, 'api/images/celebrations/bounty.png', 1);
+
 
 -- Orders
-INSERT INTO orders (id, status) VALUES (DEFAULT, 'IN_PROGRESS');
-INSERT INTO orders (id, status) VALUES (DEFAULT, 'IN_PROGRESS');
+-- INSERT INTO orders (id, status) VALUES (DEFAULT, 'IN_PROGRESS');
+-- INSERT INTO orders (id, status) VALUES (DEFAULT, 'IN_PROGRESS');
 
 -- Product Entries
-INSERT INTO product_entries (id, product_id, amount) VALUES (DEFAULT, 3, 6);
-INSERT INTO product_entries (id, product_id, amount) VALUES (DEFAULT, 5, 7);
-INSERT INTO product_entries (id, product_id, amount) VALUES (DEFAULT, 4, 2);
-INSERT INTO product_entries (id, product_id, amount) VALUES (DEFAULT, 3, 3);
-INSERT INTO product_entries (id, product_id, amount) VALUES (DEFAULT, 1, 5);
-INSERT INTO product_entries (id, product_id, amount) VALUES (DEFAULT, 2, 2);
+-- INSERT INTO product_entries (id, product_id, amount) VALUES (DEFAULT, 3, 6);
+-- INSERT INTO product_entries (id, product_id, amount) VALUES (DEFAULT, 5, 7);
+-- INSERT INTO product_entries (id, product_id, amount) VALUES (DEFAULT, 4, 2);
+-- INSERT INTO product_entries (id, product_id, amount) VALUES (DEFAULT, 3, 3);
+-- INSERT INTO product_entries (id, product_id, amount) VALUES (DEFAULT, 1, 5);
+-- INSERT INTO product_entries (id, product_id, amount) VALUES (DEFAULT, 2, 2);
 
 -- Orders Entries (mapping orders to their respective product entries)
-INSERT INTO orders_entries (order_id, entries_id) VALUES (1, 1);
-INSERT INTO orders_entries (order_id, entries_id) VALUES (1, 2);
-INSERT INTO orders_entries (order_id, entries_id) VALUES (1, 3);
-INSERT INTO orders_entries (order_id, entries_id) VALUES (1, 4);
-INSERT INTO orders_entries (order_id, entries_id) VALUES (2, 5);
-INSERT INTO orders_entries (order_id, entries_id) VALUES (2, 6);
+-- INSERT INTO orders_entries (order_id, entries_id) VALUES (1, 1);
+-- INSERT INTO orders_entries (order_id, entries_id) VALUES (1, 2);
+-- INSERT INTO orders_entries (order_id, entries_id) VALUES (1, 3);
+-- INSERT INTO orders_entries (order_id, entries_id) VALUES (1, 4);
+-- INSERT INTO orders_entries (order_id, entries_id) VALUES (2, 5);
+-- INSERT INTO orders_entries (order_id, entries_id) VALUES (2, 6);
